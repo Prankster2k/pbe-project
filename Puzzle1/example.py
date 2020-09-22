0d30c11c630aa0383fe7df0e651f5c003c3fb0ee
Jesus <jesusvico@gmail.com>
2020-09-22 19:36:23 +0200

    Puzzle1 done

---
+++ Puzzle1/example.py
@@ -0,0 +1,41 @@
+"""
+This example shows connecting to the PN532 with I2C (requires clock
+stretching support), SPI, or UART. SPI is best, it uses the most pins but
+is the most reliable and universally supported.
+After initialization, try waving various 13.56MHz RFID cards over it!
+"""
+
+import board
+import busio
+from digitalio import DigitalInOut
+import time
+
+#
+# NOTE: pick the import that matches the interface being used
+#
+from adafruit_pn532.i2c import PN532_I2C
+
+# I2C connection:
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# With I2C, we recommend connecting RSTPD_N (reset) to a digital pin for manual
+# harware reset
+#reset_pin = DigitalInOut(board.D6)
+# On Raspberry Pi, you must also connect a pin to P32 "H_Request" for hardware
+# wakeup! this means we don't need to do the I2C clock-stretch thing
+#req_pin = DigitalInOut(board.D12)
+pn532 = PN532_I2C(i2c, debug=False)
+
+#ic, ver, rev, support = pn532.firmware_version
+#print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+
+# Configure PN532 to communicate with MiFare cards
+pn532.SAM_configuration()
+
+print("Waiting for RFID/NFC card...")
+while True:
+    # Check if a card is available to read
+    uid = pn532.read_passive_target(timeout=0.5)
+    # Try again if no card is available.
+    if uid is not None:
+        print("Found card with UID:", [hex(i) for i in uid])
